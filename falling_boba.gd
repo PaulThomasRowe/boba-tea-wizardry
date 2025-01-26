24c7de10ddccf5e13a0f3778539ab2272d2448da
Ming-Hua Tsai <48788292+alimhtsai@users.noreply.github.com>
2025-01-26 11:47:48 -0800

    feat: make boba only drop inside the cup (#57)

--- falling_boba.gd
+++ falling_boba.gd
@@ -4,6 +4,8 @@ extends RigidBody2D
 
 @export var min_fall_speed: float = .001
 @export var max_fall_speed: float = .1
+var left_boundary: float
+var right_boundary: float
 
 func _ready():
 	# Set random fall speed
@@ -11,6 +13,23 @@ func _ready():
 	linear_velocity = Vector2(0, fall_speed)
 
 func _process(delta):
+	# Check if the boba has reached the left or right boundary
+	if position.x <= left_boundary:
+		position.x = left_boundary
+		linear_velocity.x = 0
+	elif position.x >= right_boundary:
+		position.x = right_boundary
+		linear_velocity.x = 0
+	
 	# Remove the object when it's below the bottom of the screen
 	if position.y > get_viewport_rect().size.y + 50:
 		queue_free()
+
+func _integrate_forces(state):
+	# Add a small random horizontal velocity for more interesting movement
+	if state.linear_velocity.x == 0:
+		state.linear_velocity.x = randf_range(-20, 20)
+
+func set_boundaries(left: float, right: float):
+	left_boundary = left
+	right_boundary = right
