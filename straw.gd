7a1ee4365440ed322f70e07344acb8b8fbb95a46
Jacques Zwielich <35075165+JAZwielich@users.noreply.github.com>
2025-01-26 16:23:31 -0800

    Feature/improve straw and make dangerous (#73)
    * Update straw art and make lowering speed gradual
    
    * Improve gradual transition of falling speed
    
    * Make straw object collidable; redo rotation logic
    
    * Moved straw to a different layer
    
    * Player object can correctly tell the diffence between straw and boba
    
    * Functioning straw, yay
    
    ---------
    
    Co-authored-by: Paul Thomas Rowe <paul@gnarlywoodgroup.com>
    Co-authored-by: 羅寶堂 <ptr@prowe.dev>

--- straw.gd
+++ straw.gd
@@ -1,32 +1,44 @@
-extends RigidBody2D
+extends StaticBody2D
 
 var swing_amplitude = 20.0
-var swing_speed = 2.0
-var downward_speed = 20.0
+var swing_speed = 0.5
+var downward_speed = 45.0
 
-var min_rotation = 110
-var max_rotation = 160
-var clockwise = false
+var max_rotation = 15
+var clockwise = true
 
 func _ready():
 	return
 
 func _process(delta):
-	position.y += downward_speed * delta
-	if clockwise && rotation_degrees < min_rotation:
-		clockwise = false
-	elif !clockwise && rotation_degrees > max_rotation:
-		clockwise = true
+	#if (fmod(rotation_degrees, 360) < 1):
+	#	rotation_degrees += 360
+	#print(position.y)
+	
+	if (position.y < -10):
+		position.y += downward_speed * delta * 0.3
+	elif (position.y < -200):
+		position.y += downward_speed * delta * 0.5
+	elif (position.y < -400):
+		position.y += downward_speed * delta * 0.7
+	elif (position.y < -500):
+		position.y += downward_speed * delta * 0.9
+	elif (position.y < -600):
+		position.y += downward_speed * delta * 1.2
+	
+	if (abs(fmod(rotation_degrees, 360)) > max_rotation):
+		clockwise = !clockwise
 	
 	if clockwise:
-		rotation_degrees -= 45 * delta
+		rotation_degrees -= 45 * delta * swing_speed
 	else: 
-		rotation_degrees += 45 * delta
+		rotation_degrees += 45 * delta * swing_speed
 	
 func start(pos):
-	position = pos
-	show()
+	#position = pos
+	#show()
 	$CollisionShape2D.disabled = false
-	$AnimatedSprite2D.play()
-	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
-	$AnimatedSprite2D.animation = mob_types.pick_random()
+	#$AnimatedSprite2D.play()
+	#var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
+	#$AnimatedSprite2D.animation = mob_types.pick_random()
+	
